3d2e7f76c08082016e0bdcd02933b90ff78ff56c
pawara101 <pawara101dassanayake@gmail.com>
2023-06-27 09:04:21 +0530

    R study codes | 2023-06-27

--- DistriutionPropotion.R
+++ DistriutionPropotion.R
@@ -6,7 +6,7 @@ n_values <- c(1, 10, 30, 50, 100)
 p_values <- c(0.5, 0.7, 0.1)
 
 # Adjust figure margins
-par(mar = c(2.5, 2.5, 1, 1))  # Adjust the margins as needed (bottom, left, top, right)
+par(mar = c(2.5, 2.5, 1, 1))  # Adjust the margins as needed (bottom, left, top, right) # nolint
 
 # Generate and plot distributions
 par(mfrow = c(length(p_values), length(n_values)))  # Set up the layout of subplots
---
+++ tutorial/r1.R
@@ -0,0 +1,20 @@
+mystring <- "Hello world"
+# My first program in R Programming
+print(mystring)
+
+## Data types
+
+### Logical
+v <- TRUE
+print((class(v)))
+
+### Vectors
+nums <- c(1,2,3,4,5)
+fruits <- c('apple','orange','mango')
+
+print(nums) # nolint
+print(fruits)
+
+### Lists
+list1 <- list(c(1,3,4,56,8),90,'pawara')
+print(list1)
